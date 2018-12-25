18128d186078b1b955f701f969a20768a3c5590a
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2018-12-26 01:57:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "c";
-var adminprefix = 'Y'
+var prefix = "$";
+var adminprefix = '$'
 
-const developers = ["472413769700474901"]
+const developers = ["464575938215280641"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
