0f9724b251edce8ffe5a3a49d834dc59f8a27cba
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2018-12-26 19:30:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'السلام عليكم') {
-    msg.reply('عليكم السلام ورحمة الله وبركاته');
+  if (msg.content === 'Myping') {
+    msg.reply('Your Ping is LOL ');
   }
 });
 
